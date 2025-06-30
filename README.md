f4fc19f0dcb4d6e825196974aee9ec3a7db9efaf
Michał Grygierzec <michal.grygierzec@protonmail.com>
2025-06-30 23:34:36 +0200

    set up basic template

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # macnix
-Set up Nix on MacOS using nix-darwin and home-manager with flakes
+
+Set up Nix on MacOS using nix-darwin and home-manager with flakes:
+
+- [home-manager instructions](https://nix-community.github.io/home-manager/index.xhtml#sec-flakes-nix-darwin-module)
+- [nix-darwin configuration options](https://nix-darwin.github.io/nix-darwin/manual/index.html)
+- [Nix cheatsheet](https://nixcademy.com/downloads/cheatsheet.pdf)
+
+## Installation
+
+[Source](https://nixcademy.com/posts/nix-on-macos/)
+
+1. Install Nix with determinate systems installer:
+
+`curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate`
+
+2. Test by running: `nix run "nixpkgs#hello"`
+
+3. Set up Mac by running: `darwin-rebuild switch --flake <flake-uri>.`
+
+`<flake-uri>` could be: `~/macnix.#myhostname`
+
+4. To update flake inputs run: `nix flake update` or more granularly: `nix flake lock --update-input <input>`
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "MacNix Darwin configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      home-manager,
+      nix-darwin,
+      self,
+      ...
+    }:
+    {
+      darwinConfigurations = let
+        myhostname = builtins.abort "TODO set correct 'myhostname'";
+        user = builtins.abort "TODO: set proper username";
+      in {
+        ${myhostname} = nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
+            (./configuration.nix { inherit (self) rev dirtyRev; })
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${user} = (./home.nix { inherit user; });
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+          ];
+        };
+      };
+    };
+}
