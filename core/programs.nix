467e8a9f3337557f484beb30d28a45eb164d0895
Michal Grygierzec <michal.grygierzec@pm.me>
2025-07-07 10:27:20 +0200

    add emacs

--- core/programs.nix
+++ core/programs.nix
@@ -1,19 +1,11 @@
 { pkgs, ... }:
 {
-  # bash = {
-  #   enable = true;
-  #   bashrcExtra = ''
-  #     export SHELL="${pkgs.bash}/bin/bash"
-  #   '';
-  # };
   bat = {
     enable = true;
     config = { pager = "less -FR"; theme = "1337"; };
   };
   bottom.enable = true;
-  # broot.enable = true;
-  # chromium.enable = true;
-  # emacs.enable = true;
+  emacs.enable = true;
   eza = {
     enable = true;
     git = true;
@@ -24,7 +16,6 @@
   jq.enable = true;
   skim.enable = true;
   tealdeer.enable = true;
-  # vscode.enable = true;
   zoxide = {
     enable = true;
     enableBashIntegration = true;
