816dadcbf6e4646f175a199441b2f09656d92c1c
Michal Grygierzec <michal.grygierzec@pm.me>
2025-07-31 17:56:33 +0200

    inline nix pkgs

--- home.nix
+++ home.nix
@@ -17,10 +17,35 @@
   home = {
     username = user;
     # file = import ./homeFile.nix { inherit config; };
-    packages =
-      import ./core/packages.nix { inherit pkgs; }
-      ++ (with pkgs; [
-      ]);
+    packages = (with pkgs; [
+      azure-cli
+      delta
+      du-dust
+      duf
+      eslint
+      fd
+      font-awesome
+      git-crypt
+      glab
+      neofetch
+      nil
+      nixfmt-classic
+      nodePackages.nodejs
+      pnpm
+      # Python LSP
+      python312Packages.pylsp-mypy
+      python312Packages.pylsp-rope
+      python312Packages.python-lsp-ruff
+      python312Packages.python-lsp-server
+      ripgrep
+      rustup
+      screen
+      shellcheck
+      tokei
+      typescript
+      typescript-language-server
+      vscode-langservers-extracted
+    ]);
     sessionVariables = {
       # Other env vars here cause issues during fish shell startup
       EDITOR = "hx";
