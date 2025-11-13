8ba53bf67cd6a9eed1e5873ca13846f5c8be68a0
Michał Grygierzec <michal.grygierzec@protonmail.com>
2025-11-14 00:21:07 +0100

    Refactor ci_configure_flake script

--- ci_configure_flake.sh
+++ ci_configure_flake.sh
@@ -1,16 +1,20 @@
-#! /usr/bin/env bash
-# flake
-nix run nixpkgs#perl -- -i -wne 's/^(\s*)loginwindowText = builtins\.abort.*$/${1}loginwindowText = "login-mock";/; print' flake.nix
-nix run nixpkgs#perl -- -i -wne 's/^(\s*)hostname = builtins\.abort.*$/${1}hostname = "ci-host";/; print' flake.nix
-nix run nixpkgs#perl -- -i -wne 's/^(\s*)user = builtins\.abort.*$/${1}user = "test";/; print' flake.nix
-nix run nixpkgs#perl -- -i -wne 's/^(\s*)windowManager = builtins\.abort.*$/${1}windowManager = "leftwm";/; print' flake.nix
+#!/usr/bin/env bash
+set -exuo pipefail
+
+perl -i -wpl -e '
+  s/^(\s*)loginwindowText = builtins\.abort.*$/${1}loginwindowText = "login-mock";/;
+  s/^(\s*)hostname = builtins\.abort.*$/${1}hostname = "ci-host";/;
+  s/^(\s*)user = builtins\.abort.*$/${1}user = "test";/;
+  s/^(\s*)windowManager = builtins\.abort.*$/${1}windowManager = "leftwm";/;
+' flake.nix
+
+perl -i -wpl -e 's/^(\s*)# CIFIXME_TEST.*$/${1}users.users.test.isNormalUser = true;/;' sage_configuration.nix
 
 echo '{ ... }: {  fileSystems = { "/".device = "/dev/fake1"; "/nix".device = "/dev/fake1"; "/home".device = "/dev/fake1"; "/data".device = "/dev/fake1"; "/snapshots".device = "/dev/fake1"; "/swap".device = "/dev/fake1"; }; }' > hardware-configuration.nix
+
 git add hardware-configuration.nix
 
 echo '[]' > keys.nix
 git add keys.nix
 
-# configuration
-nix run nixpkgs#perl -- -i -wne 's/^(\s*)# CIFIXME_TEST.*$/${1}users.users.test.isNormalUser = true;/; print' sage_configuration.nix
-
+echo 'Done';
